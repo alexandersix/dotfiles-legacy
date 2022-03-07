2ebd8e43284bdda1d2d92be1562af62d1dbeaa2c
Alexander Six <alexanderhsix@gmail.com>
2022-03-06 22:36:52 -0500

    Update cheatsheet to handle multi-word edge case for languages

--- bin/tmux-cht.sh
+++ bin/tmux-cht.sh
@@ -9,8 +9,8 @@ fi
 read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.tmux-cht-languages; then
-    $query = `echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    concat=`echo $query | tr ' ' '+'`
+    tmux neww bash -c "echo \"curl cht.sh/$selected/$concat/\" & curl cht.sh/$selected/$concat & while [ : ]; do sleep 1; done"
 else
     tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
 fi

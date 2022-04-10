8b613938beb5fc6f7090189f5e5e4c84cbaaf9c5
Alexander Six <alexanderhsix@gmail.com>
2022-04-10 11:05:26 -0400

    Additions to neo-tree config

--- nvim/lua/configs/neo-tree.lua
+++ nvim/lua/configs/neo-tree.lua
@@ -86,12 +86,11 @@ function M.config()
     filesystem = {
       filtered_items = {
         visible = false,
-        hide_dotfiles = true,
+        hide_dotfiles = false,
         hide_gitignored = false,
         hide_by_name = {
           ".DS_Store",
           "thumbs.db",
-          "node_modules",
           "__pycache__",
         },
       },

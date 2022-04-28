e71a2f219c730c357f2496fc2b64e3570c6ff8ac
Alexander Six <alexanderhsix@gmail.com>
2022-04-28 08:34:30 -0400

    Adjusts ToggleTerm configuration

--- nvim/lua/configs/toggleterm.lua
+++ nvim/lua/configs/toggleterm.lua
@@ -7,16 +7,22 @@ function M.config()
   end
 
   toggleterm.setup(require("core.utils").user_plugin_opts("plugins.toggleterm", {
-    size = 10,
+    size = function(term)
+      if term.direction == "horizontal" then
+        return 15
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+    end,
     open_mapping = [[<c-\>]],
     hide_numbers = true,
     shade_filetypes = {},
     shade_terminals = true,
     shading_factor = 2,
-    start_in_insert = true,
+    start_in_insert = false,
     insert_mappings = true,
     persist_size = true,
-    direction = "float",
+    direction = "horizontal",
     close_on_exit = true,
     shell = vim.o.shell,
     float_opts = {
